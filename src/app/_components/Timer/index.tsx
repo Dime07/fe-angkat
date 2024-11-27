f32f8164b5e8a20d2ff960eacda83bf021584964
Dime07 <dimasbkt7@gmail.com>
2024-11-27 22:41:45 +0700

    feat: create timer

--- src/app/_components/Timer/index.tsx
+++ src/app/_components/Timer/index.tsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+import duration from "dayjs/plugin/duration";
+import dayjs from "dayjs";
+
+dayjs.extend(duration);
+
 const Timer = () => {
+  const [timeSpend, setTimeSpend] = useState<number>(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeSpend((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Timer</h1>
-    </div>
+    <p className="text-center text-2xl font-bold text-secondary-950">
+      {dayjs.duration(timeSpend, "seconds").format("HH:mm:ss")}
+    </p>
   );
 };
 
