903a1bc3ab8b111f9e9a782b35ddfcf926555de7
Dime07 <dimasbkt7@gmail.com>
2024-12-13 21:47:15 +0700

    fix: empty state community

--- src/app/_components/Content/Community/WorkoutList/index.tsx
+++ src/app/_components/Content/Community/WorkoutList/index.tsx
@@ -6,24 +6,24 @@ const WorkoutList = async () => {
   const workout = workoutRes.data;
 
   return (
-    <div className="mt-3 grid grid-cols-2 gap-3">
+    <>
       {workout?.length > 0 ? (
-        <>
+        <div className="mt-3 grid grid-cols-2 gap-3">
           {workout?.map((workout, i) => (
             <WorkoutCard key={i} workout={workout} showLikeButton />
           ))}
-        </>
+        </div>
       ) : (
         <WorkoutListEmptyState />
       )}
-    </div>
+    </>
   );
 };
 
 const WorkoutListEmptyState = () => {
   return (
     <div className="flex justify-center items-center h-20">
-      <p className="text-gray-400">No workout found</p>
+      <p className="text-gray-400">No community post found</p>
     </div>
   );
 };
